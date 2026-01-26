87c9e91484245f36f6e6910ee4ed84df6656193c
Justin Holley <jholley6@gmail.com>
2026-01-25 17:28:35 -0700

    Add Genome Class in genome.py

---
+++ genome.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class Genome:
+    """Genome Class
+    This class is for generating and capturing genes of organisms. Each Genome
+    contains information that can later be interpreted inside an organism class
+    to produce that organisms phenotypes.
+    """
+
+    def __init__(self, genes=None, size=6):
+        self.genes = genes if genes is not None else np.random.rand(size)
+
+        def mutate(self, rate=0.05, std_dev=0.1):
+            '''Mutate Function, can be called to mutate genes
+            Args:
+                param1: rate (float) between 0 and 1, inclusive. Represents the
+                mutation rate percentage for any given gene to mutate.
+                default is 0.05 (5%)
+
+                param2: std_dev (float) represents the standard deviation from
+                the mean for a normal distribution. Impacts how much more/less
+                a genes value changed during mutation. Based on normal
+                distribution.
+
+            Returns:
+                None. Updates self.genes
+            '''
+            # determine which, if any, gene mutates witha mask
+            mutate_mask = np.random.rand(len(self.genes)) < rate
+            # adjust mutated genes based on normal distribution
+            self.genes[mutate_mask] += np.random.normal(0, std_dev,
+                                                        mutate_mask.sum())
+            # clip values to ensure they are within 0 and 1
+            np.clip(self.genes, 0, 1, self.genes)
+
+        def get_genes(self):
+            '''Return genes for this genome'''
+            return self.genes
