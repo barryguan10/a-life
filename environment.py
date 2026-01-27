d752468871539c167c447748d08d66b84357a963
Justin Holley <jholley6@gmail.com>
2026-01-26 23:09:39 -0700

    Added env grid with energy locations to init

--- environment.py
+++ environment.py
@@ -1,4 +1,24 @@
+import globals as gl
+import numpy as np
+from random import randrange
+
+
 class Environment:
+    def __init__(self, grid_width, grid_height, energy_density=25):
+        self.grid = np.zeros((grid_width, grid_height))
+
+        # Create Energy Sources
+        for _ in range(grid_width*grid_height*energy_density//100):
+            x = randrange(grid_width)
+            y = randrange(grid_height)
+            self.grid[x, y] = gl.ENERGY_CELL
+
+        def get_energy(self, x, y):
+            if self.grid[x, y] == gl.ENERGY_CELL:
+                self.grid[x, y] = gl.EMPTY_CELL
+                return 1
+            return 0
+
     def create_new_environment(self):
         pass
 
