c80906879d023b58ffcbb84d20491efe48af3f90
Justin Holley <jholley6@gmail.com>
2026-01-26 23:06:54 -0700

    Created file for Global variables

---
+++ globals.py
@@ -0,0 +1,13 @@
+
+# Simultion Window information
+GRID_WIDTH = 25
+GRID_HEIGHT = 25
+CELL_SIZE = 25
+WINDOW_SIZE = (GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE)
+CAPTION_PLAY = "A-Life Simulation: PLAYING"
+CAPTION_PAUSED = "A-Life Simulation: PAUSED"
+FPS = 60
+
+# Represents cell state in grid
+EMPTY_CELL = 0
+ENERGY_CELL = 1
