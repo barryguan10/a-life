a07f2a4b8c4d040a890e8434f59c6e262f6f2fc6
Justin Holley <jholley6@gmail.com>
2026-01-27 20:45:46 -0700

    Organism Color now dicated by organism trait

--- environment.py
+++ environment.py
@@ -5,12 +5,13 @@ MAX_FOOD = 10 # Max food can be in a cell to stay within bounds of RGB color syn
 TARGET_FOOD = 5 # Aim to fluctuate around the midpoint
 FOOD_PROBABILITY = 0.05 # Chance that a cell will start with food
 
+
 class Environment:
     """Represents the 2d grid the organisms exist in"""
     def __init__(self, width, height):
         """
         Docstring for __init__
-        
+
         :param width: integer representing number of horizontal tiles
         :param height: integer representing number of vertical tiles
         """
@@ -19,11 +20,10 @@ class Environment:
 
         # list of the organisms in the environment
         self.organisms = []
-        
-        # create an organism at a random spot within the bounds of the grid and set color to red for visibility
+
+        # create an organism at a random spot within the bounds of the grid
         self.organisms.append(Organism(x_pos=random.randint(0, width-1), y_pos=random.randint(0, height-1)))
-        self.organisms[0].color = (255,0,0)
-        
+
         # initialize a 2d grid to represent environment
         self.grid = []
         for x in range(width):
@@ -34,11 +34,11 @@ class Environment:
                     food = TARGET_FOOD
                 else:
                     food = 0.0
-                
+
                 # each cell is a dictionary with amount of food, potentially add more data later
                 column.append({"food": food})
-            
-            self.grid.append(column) 
+
+            self.grid.append(column)
 
     def create_new_environment(self):
         pass
@@ -46,14 +46,14 @@ class Environment:
     def update_environment(self):
         """
         Docstring for update_environment
-        
+
         Updates environment in each step, currently fluctuates food amount only
         """
         for x in range(self.width):
             for y in range(self.height):
-                fluctuation = random.uniform(-0.02,0.02)
+                fluctuation = random.uniform(-0.02, 0.02)
                 self.grid[x][y]["food"] = max(0, min(MAX_FOOD, self.grid[x][y]["food"] + fluctuation))
-                
+
     def get_organisms(self):
         # returns lists of organisms in environment
         return self.organisms
