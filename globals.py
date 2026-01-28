821c90e9c50dcb4daf002ddb211f6aefc56b4fd0
Justin Holley <jholley6@gmail.com>
2026-01-27 22:41:53 -0700

    Removed variables in Globals, can be scoped to main

--- globals.py
+++ globals.py
@@ -1,13 +1,9 @@
 
-# Simultion Window information
-GRID_WIDTH = 25
-GRID_HEIGHT = 25
-CELL_SIZE = 25
-WINDOW_SIZE = (GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE)
-CAPTION_PLAY = "A-Life Simulation: PLAYING"
-CAPTION_PAUSED = "A-Life Simulation: PAUSED"
-FPS = 60
-
-# Represents cell state in grid
-EMPTY_CELL = 0
-ENERGY_CELL = 1
+# # Simultion Window information
+# GRID_WIDTH = 25
+# GRID_HEIGHT = 25
+# CELL_SIZE = 25
+# WINDOW_SIZE = (GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE)
+# CAPTION_PLAY = "A-Life Simulation: PLAYING"
+# CAPTION_PAUSED = "A-Life Simulation: PAUSED"
+# FPS = 60
