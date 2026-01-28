d121b38a5c5d38e5f5fec4970ecc6a70c5aa8ca4
Justin Holley <jholley6@gmail.com>
2026-01-27 22:42:24 -0700

    Removed globals.py, no longer needed

--- globals.py
+++
@@ -1,9 +0,0 @@
-
-# # Simultion Window information
-# GRID_WIDTH = 25
-# GRID_HEIGHT = 25
-# CELL_SIZE = 25
-# WINDOW_SIZE = (GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE)
-# CAPTION_PLAY = "A-Life Simulation: PLAYING"
-# CAPTION_PAUSED = "A-Life Simulation: PAUSED"
-# FPS = 60
