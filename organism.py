5878230bf45758accf3686bceb75b8516324cec4
Justin Holley <jholley6@gmail.com>
2026-01-29 09:43:23 -0700

    changed indexing for decoded genome

--- organism.py
+++ organism.py
@@ -34,11 +34,11 @@ class Organism:
         val = 1.0
         rgb = hsv_to_rgb(hue, sat, val)
         # keep speed between 0 and 10
-        speed = int((genes[2] * 10))
+        speed = int((genes[1] * 10))
         # based on speed, value between 1 and 5
-        metabolism = int(genes[2] * 5) + 1
+        metabolism = int(genes[1] * 5) + 1
         # total starting energy when born
-        energy = int(genes[3] * 100)
+        energy = int(genes[2] * 100)
 
         return {
             "color": tuple([x * 255 for x in rgb]),
