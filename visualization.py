cc691b7178545c720ded214a39a6950709b5b22e
Justin Holley <jholley6@gmail.com>
2026-01-29 18:11:40 -0700

    linting on visualization.py

--- visualization.py
+++ visualization.py
@@ -1,8 +1,9 @@
 import pygame
 
+
 class PyVisualization:
 
-    def __init__(self,screen_height,screen_width,tile_size):
+    def __init__(self, screen_height, screen_width, tile_size):
         pygame.init()
         self.screen_height = screen_height
         self.screen_width = screen_width
@@ -12,34 +13,38 @@ class PyVisualization:
         self.tile_size
 
         # Color RGB
-        self.BLACK = (0,0,0,)
-        self.GREY = (128,128,128)
+        self.BLACK = (0, 0, 0,)
+        self.GREY = (128, 128, 128)
         self.GREEN = (0, 255, 0)
         self.screen = pygame.display.set_mode((screen_width, screen_height))
 
-
-    def draw_grid(self,positions = None): 
+    def draw_grid(self, positions=None):
         '''
         Draws the positions of "Alive" cells
-        Argument: positions is a set of alive cells only (no need to look at every cell)
+        Argument: positions is a set of alive cells only (no need to look at
+        every cell)
         '''
         self.screen.fill(self.GREY)
 
         # draw alive cells
         if positions:
             for position in positions:
-                col, row = position         # It's "Col, Row" due to X, Y (think 2D-Cartesian Coordinates)
-                top_left = (col * self.tile_size, row *self.tile_size)
-                        # Draw a Rectangle Object. RectValue is top left position and then 
-                        # rectangle width and height from that point. *top_left is just unpacking 
-                pygame.draw.rect(self.screen, self.GREEN, (*top_left, self.tile_size, self.tile_size))
+                col, row = position
+                top_left = (col * self.tile_size, row * self.tile_size)
+                pygame.draw.rect(self.screen, self.GREEN, (*top_left,
+                                                           self.tile_size,
+                                                           self.tile_size))
 
         # Draw Grid Lines
-        for row in range(self.height_cell_count): 
-            pygame.draw.line(self.screen, self.BLACK, (0, row * self.tile_size), (self.screen_width, row * self.tile_size))
+        for row in range(self.height_cell_count):
+            pygame.draw.line(self.screen, self.BLACK,
+                             (0, row * self.tile_size),
+                             (self.screen_width, row * self.tile_size))
 
         for col in range(self.width_cell_count):
-            pygame.draw.line(self.screen, self.BLACK, (col * self.tile_size, 0), (col * self.tile_size, self.screen_height))
+            pygame.draw.line(self.screen, self.BLACK,
+                             (col * self.tile_size, 0),
+                             (col * self.tile_size, self.screen_height))
 
 
 def main():
@@ -52,9 +57,6 @@ def main():
                 running = False
         viz.draw_grid()
         pygame.display.update()
-
-    
-
 
 
 if __name__ == "__main__":
