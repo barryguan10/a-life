21df3071c6e48f0667ba555e00a4ae10a8a1f614
gabri <gabrieljeanbertrand@gmail.com>
2026-01-31 22:06:28 -0800

    Fixed formatting

--- environment.py
+++ environment.py
@@ -3,12 +3,13 @@ from organism import Organism
 
 # Max food can be in a cell to stay within bounds of RGB color syntax
 MAX_FOOD = 10
-TARGET_FOOD = 5             # Aim to fluctuate around the midpoint
-FOOD_PROBABILITY = 0.05     # Chance that a cell will start with food
+TARGET_FOOD = 5  # Aim to fluctuate around the midpoint
+FOOD_PROBABILITY = 0.05  # Chance that a cell will start with food
 
 
 class Environment:
     """Represents the 2d grid the organisms exist in"""
+
     def __init__(self, width, height):
         """
         Docstring for __init__
@@ -23,8 +24,12 @@ class Environment:
         self.organisms = []
 
         # create an organism at a random spot within the bounds of the grid
-        self.organisms.append(Organism(x_pos=random.randint(0, width-1),
-                                       y_pos=random.randint(0, height-1)))
+        self.organisms.append(
+            Organism(
+                x_pos=random.randint(0, width - 1),
+                y_pos=random.randint(0, height - 1)
+            )
+        )
 
         # initialize a 2d grid to represent environment
         self.grid = []
@@ -73,44 +78,56 @@ class Environment:
         for x in range(self.width):
             for y in range(self.height):
                 fluctuation = random.uniform(-0.02, 0.02)
-                self.grid[x][y]["food"] = max(0, min(MAX_FOOD,
-                                                     self.grid[x][y]["food"] +
-                                                     fluctuation))
+                self.grid[x][y]["food"] = max(
+                    0, min(MAX_FOOD, self.grid[x][y]["food"] + fluctuation)
+                )
                 if self.grid[x][y]["occupancy"] != 2:
-                    self.grid[x][y]["occupancy"] = 1 if self.grid[x][y]["food"]> 0 else 0
+                    self.grid[x][y]["occupancy"] = (
+                        1 if self.grid[x][y]["food"] > 0 else 0
+                    )
         self.place_organisms_grid()
 
     def get_organisms(self):
         # returns lists of organisms in environment
         return self.organisms
-    
-    def get_surroundings(self,organism: Organism):
+
+    def get_surroundings(self, organism: Organism):
         """
         A function to return the surroundings of an organism
-        
+
         :param self: The environment instance
         :param organism: The organism for the surroundings to be returned
         """
-        #Get the current position of the organism
+        # Get the current position of the organism
         org_x, org_y = organism.get_pos()
-        
+
         # Positions to check, can be replaced in the future with
         # the organism's sight
-        surr_positions = [(-1,-1),(0,-1), (1,-1), (-1,0), (1,0)
-                          ,(-1,1), (0,1), (1,1)]
-        
+        surr_positions = [
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+            (-1, 0),
+            (1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        ]
+
         surr_items = list()
-        
+
         for pos in surr_positions:
             x_offset, y_offset = pos
 
-            # Check each spot in the grid to see there is an object there and if so add its location and what
+            # Check each spot in the grid to see there is an object there and
+            # if so add its location and what
             # it is to the positions
-            if self.grid[org_x + x_offset][org_y+y_offset] != 0:
-                surr_items.append(((org_x + x_offset,org_y+y_offset),self.grid[org_x + x_offset][org_y+y_offset]))
-
+            if self.grid[org_x + x_offset][org_y + y_offset] != 0:
+                surr_items.append(
+                    (
+                        (org_x + x_offset, org_y + y_offset),
+                        self.grid[org_x + x_offset][org_y + y_offset],
+                    )
+                )
 
         return surr_items
-
-
-
