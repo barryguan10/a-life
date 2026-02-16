f730b486d6eed5e1076ed7cac188c06b659a35d5
Justin Holley <jholley6@gmail.com>
2026-02-16 11:29:46 -0700

    Added update hover method

--- buttons.py
+++ buttons.py
@@ -5,7 +5,9 @@ class Button:
     def __init__(self, rect, text):
         self.rect = pygame.Rect(rect)
         self.text = text
-        self.color = (255, 0, 0)
+        self.hover_color = (125, 0, 0)
+        self.main_color = (255, 0, 0)
+        self.color = self.main_color
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 25)
         self.text_surface = self.font.render(self.text, True, self.text_color)
@@ -19,3 +21,9 @@ class Button:
         self.text = text
         self.text_surface = self.font.render(self.text, True, self.text_color)
         self.text_rect = self.text_surface.get_rect(center=self.rect.center)
+
+    def update_hover(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            self.color = self.hover_color
+        else:
+            self.color = self.main_color
