f1653edbccde176e7227c42d48148965f80c38ce
Nicole Rishwain <nicolerishwain@gmail.com>
2026-02-18 13:29:54 -0800

    passed start_plant and start_organisms to environment

--- overseer.py
+++ overseer.py
@@ -1,12 +1,27 @@
 import environment
+import editableParameters
 
 
 class Overseer:
     def __init__(self, width, height):
-        self.environment_instance = environment.Environment(width, height)
+        self.width = width
+        self.height = height
+        self.editable_parameters = editableParameters.EditableParameters()
 
-    def new_simulator(self):
-        self.environment_instance = environment.Environment()
+        self.environment_instance = environment.Environment(
+            width,
+            height,
+            self.editable_parameters.get_start_plants(),
+            self.editable_parameters.get_start_organisms()
+        )
+
+    def reset_simulation(self):
+        self.environment_instance = environment.Environment(
+            self.width,
+            self.height,
+            self.editable_parameters.get_start_plants(),
+            self.editable_parameters.get_start_organisms()
+        )
 
     def run_simulation(self, number_of_steps):
         for i in range(number_of_steps):
