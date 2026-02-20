be272c027b549b07c610bd3c12947a5175724ec7
gabri <gabrieljeanbertrand@gmail.com>
2026-02-19 17:18:24 -0800

    Added Docstrings

--- overseer.py
+++ overseer.py
@@ -33,11 +33,17 @@ class Overseer:
         # call display function to draw environment
 
     def save(self, filename="save.json"):
+        """
+        Method to save objects of the simulation to a JSON File 
+        """
         data = self.environment_instance.to_dictionary()
         with open(filename, "w") as f:
             json.dump(data, f)
 
     def load(self, filename="save.json"):
+        """
+        Method to load the simulation from a JSON file
+        """
         with open(filename, "r") as f:
             data = json.load(f)
         self.environment_instance = environment.Environment.from_dictionary(data)
